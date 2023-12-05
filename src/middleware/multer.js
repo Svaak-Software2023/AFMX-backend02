d0a06c20a097ee19551d96b200063c4363e264c9
iftekhar841 <mdiftekhar841@gmail.com>
2023-12-05 11:34:35 +0530

    Suggested changes

--- src/middleware/multer.js
+++ src/middleware/multer.js
@@ -7,7 +7,7 @@ const destinationPathClientImage = path.join(
 );
 const destinationPathbannerImage = path.join(
   __dirname,
-  "../public/advertiesImages"
+  "../public/advertiseImages"
 );
 const destinationPathJoinImage = path.join(__dirname, "../public/joinImages");
 const destinationPathProductImage = path.join(__dirname, "../public/productImages");
@@ -19,7 +19,7 @@ const storage = multer.diskStorage({
     // check the fieldname to determine the destination path
     if (file.fieldname === "clientProfileImage") {
       destinationPath = destinationPathClientImage;
-    } else if (file.fieldname === "advertiesImage") {
+    } else if (file.fieldname === "advertiseImage") {
       destinationPath = destinationPathbannerImage;
     } else if (file.fieldname === "afmxJoinImageVideo") {
       destinationPath = destinationPathJoinImage;
