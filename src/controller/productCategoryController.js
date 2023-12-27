905cea9d97f90dae4892c03fba6217a5be996e82
iftekhar841 <mdiftekhar841@gmail.com>
2023-12-27 17:27:33 +0530

    Adding the product category get api's

--- src/controller/productCategoryController.js
+++ src/controller/productCategoryController.js
@@ -1,17 +1,34 @@
-const productCategoryService = require('../services/productCategoryService');
+const productCategoryService = require("../services/productCategoryService");
 
-const addProductCategory = async(req, res) => {
-    try {
-        // Handle the product category response.
-        const categoryResponse = await productCategoryService.addProductCategory(req.body);
+const addProductCategory = async (req, res) => {
+  try {
+    // Handle the product category response.
+    const categoryResponse = await productCategoryService.addProductCategory(
+      req.body
+    );
 
-        return res.json({ message: "Category Added !", categoryResponse});
-        
-    } catch (error) {
-        return res.status(500).json({ error: error.message });
-    }
-}
+    return res.json({ message: "Category Added !", categoryResponse });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+const getProductCategory = async (req, res) => {
+  try {
+    const productCategoryResponse =
+      await productCategoryService.getProductCategory();
+    return res
+      .status(200)
+      .json({
+        message: "Product category retrieved successfully",
+        productCategoryResponse,
+      });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
 
 module.exports = {
-    addProductCategory,
-}
+  addProductCategory,
+  getProductCategory,
+};
--- src/services/productCategoryService.js
+++ src/services/productCategoryService.js
@@ -1,39 +1,52 @@
-const ProductCategoryModel = require('../model/productCategoryModel');
+const { errorMsg } = require("../const/errorHelper");
+const ProductCategoryModel = require("../model/productCategoryModel");
 
+const addProductCategory = async (categoryDetails) => {
+  const {
+    productCategoryName,
+    productCategoryDescription,
+    createdDate,
+    updatedDate,
+    isActive,
+  } = categoryDetails;
 
-const addProductCategory = async(categoryDetails) => {
-    const {
-        productCategoryName,
-        productCategoryDescription,
-        createdDate,
-        updatedDate,
-        isActive
-    } = categoryDetails;
-
-    const existingCategory = await ProductCategoryModel.findOne({
-         productCategoryName: { $regex: new RegExp(`^${productCategoryName}$`, 'i') } 
-        });
+  const existingCategory = await ProductCategoryModel.findOne({
+    productCategoryName: {
+      $regex: new RegExp(`^${productCategoryName}$`, "i"),
+    },
+  });
 
   if (existingCategory) {
     throw new Error(`Category '${productCategoryName}' already exists`);
   }
-  
-    // Fetch count of product category
-    const productCategory = await ProductCategoryModel.countDocuments();
 
-    const categoryData = new ProductCategoryModel({
-        productCategoryId: productCategory + 1,
-        productCategoryName,
-        productCategoryDescription,
-        createdDate,
-        updatedDate,
-        isActive
-    })
+  // Fetch count of product category
+  const productCategory = await ProductCategoryModel.countDocuments();
 
-    const newProductCategory = await categoryData.save();
-    return newProductCategory;
-}
+  const categoryData = new ProductCategoryModel({
+    productCategoryId: productCategory + 1,
+    productCategoryName,
+    productCategoryDescription,
+    createdDate,
+    updatedDate,
+    isActive,
+  });
+
+  const newProductCategory = await categoryData.save();
+  return newProductCategory;
+};
+
+const getProductCategory = async () => {
+  const category = await ProductCategoryModel.find({});
+
+  if (!category) {
+    throw new Error(errorMsg.FETCH_USERS_FAILED);
+  }
+
+  return category;
+};
 
 module.exports = {
-    addProductCategory,
-}
+  addProductCategory,
+  getProductCategory,
+};
