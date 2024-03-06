2500892abe56f5d3139df886afa30121d4d34fb2
iftekhar841 <mdiftekhar841@gmail.com>
2024-03-06 13:05:10 +0530

    Adding the functionality of product delivery address

--- src/controller/productDeliveryAddressController.js
+++ src/controller/productDeliveryAddressController.js
@@ -1,9 +1,10 @@
 const productDeliveryAddressService = require("../services/productDeliveryAddressService");
 
+// Create Delivery Address Controllers
 const createProductDeliveryAddress = async (req, res) => {
   try {
     const loggedInUser = req.decoded;
-    // Handle the product delivery address response
+    // Handle the create product delivery address response
     const productDeliveryAddressResponse =
       await productDeliveryAddressService.createProductDeliveryAddress(
         req.body,
@@ -20,57 +21,98 @@ const createProductDeliveryAddress = async (req, res) => {
   }
 };
 
+// Update Delivery Address Controllers
 const updateProductDeliveryAddress = async (req, res) => {
-    try {
-        const loggedInUser = req.decoded;
-      const updateDeliveryAddressResponse = await productDeliveryAddressService.updateProductDeliveryAddress(
-          loggedInUser,
-          req.params,
-          req.body
+  try {
+    const loggedInUser = req.decoded;
+    // Handle the update product delivery address response
+    const updateDeliveryAddressResponse =
+      await productDeliveryAddressService.updateProductDeliveryAddress(
+        loggedInUser,
+        req.params,
+        req.body
       );
-      if (updateDeliveryAddressResponse) {
-        return res
-          .status(200)
-          .json({ message: "Updated Successfully", updateDeliveryAddressResponse });
-      } else {
-        return res.status(404).json({ message: "Not Updated", updateDeliveryAddressResponse });
-      }
-    } catch (error) {
-      return res.status(500).json({ error: error.message });
+    if (updateDeliveryAddressResponse) {
+      return res.status(200).json({
+        message: "Updated Successfully",
+        updateDeliveryAddressResponse,
+      });
+    } else {
+      return res
+        .status(404)
+        .json({ message: "Not Updated", updateDeliveryAddressResponse });
     }
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
 };
 
-// Get Single Department
-const getSingleCreateProductDeliveryAddress = async (req, res) => {
-    try {
-      // Handle the get department response
-      const getSingleResponse =
-        await productDeliveryAddressService.getSingleCreateProductDeliveryAddress(req.body);
-      return res.json({
-        message: "Fetch single delivery address details successfully",
-        getSingleResponse,
-      });
-    } catch (error) {
-      return res.status(400).json({ error: error.message });
-    }
-  };
-
-
-const getAllCreateProductDeliveryAddress = async (req, res) => {
-    try {
-      const getResponse = await productDeliveryAddressService.getAllCreateProductDeliveryAddress();
-      return res.json({
-        message: "Fetch all prduct devlivery address details successfully ",
-        getResponse,
-      });
-    } catch (error) {
-      return res.status(500).json({ error: error.message });
-    }
+// Get Single Delivery Address Controllers
+const getSingleProductDeliveryAddress = async (req, res) => {
+  try {
+    const loggedInUser = req.decoded;
+    // Handle the get single delivery address response
+    const getSingleDeliveryAddressResponse =
+      await productDeliveryAddressService.getSingleProductDeliveryAddress(
+        req.params,
+        loggedInUser
+      );
+    return res.json({
+      message: "Fetch single delivery address details successfully",
+      getSingleDeliveryAddressResponse,
+    });
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
 };
-  
+
+// Get All Delivery Address Controllers
+const getAllProductDeliveryAddress = async (req, res) => {
+  try {
+    const loggedInUser = req.decoded;
+    // Handle the all product delivery address response
+    const getAllDeliveryAddressResponse =
+      await productDeliveryAddressService.getAllProductDeliveryAddress(
+        loggedInUser
+      );
+    return res.json({
+      message: "Fetch all prduct devlivery address details successfully ",
+      getAllDeliveryAddressResponse,
+    });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+// Delete Single Delivery Address Controllers
+const deleteSingleProductDeliveryAddress = async (req, res) => {
+  try {
+    const loggedInUser = req.decoded;
+    // Handle the delete single product delivery address response
+    const deleteDeliveryAddressResponse =
+      await productDeliveryAddressService.deleteSingleProductDeliveryAddress(
+        loggedInUser,
+        req.params
+      );
+    if (deleteDeliveryAddressResponse) {
+      return res.status(200).json({
+        message: "Deleted Successfully",
+        deleteDeliveryAddressResponse,
+      });
+    } else {
+      return res
+        .status(404)
+        .json({ message: "Not Deleted", deleteDeliveryAddressResponse });
+    }
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
 module.exports = {
   createProductDeliveryAddress,
   updateProductDeliveryAddress,
-  getAllCreateProductDeliveryAddress,
-  getSingleCreateProductDeliveryAddress
+  getSingleProductDeliveryAddress,
+  getAllProductDeliveryAddress,
+  deleteSingleProductDeliveryAddress,
 };
--- src/routes/productDeliveryAddressRoute.js
+++ src/routes/productDeliveryAddressRoute.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const productDeliveryAdress_route = express.Router();
 
-
 const productDeliveryAdress_controller = require("../controller/productDeliveryAddressController");
 
-const { verifyToken } = require('../middleware/auth')
+const { verifyToken } = require("../middleware/auth");
 productDeliveryAdress_route.post(
   "/address/create-address",
   verifyToken,
@@ -12,20 +11,27 @@ productDeliveryAdress_route.post(
 );
 
 productDeliveryAdress_route.patch(
-    "/address/update-address/:deliveryAddressId",
-    verifyToken,
-    productDeliveryAdress_controller.updateProductDeliveryAddress
+  "/address/update-address/:deliveryAddressId",
+  verifyToken,
+  productDeliveryAdress_controller.updateProductDeliveryAddress
 );
 
 productDeliveryAdress_route.get(
-    "/address/single-address",
-    productDeliveryAdress_controller.getSingleCreateProductDeliveryAddress
+  "/address/single-address/:deliveryAddressId",
+  verifyToken,
+  productDeliveryAdress_controller.getSingleProductDeliveryAddress
 );
 
 productDeliveryAdress_route.get(
-    "/address/all-address", 
-    productDeliveryAdress_controller.getAllCreateProductDeliveryAddress
+  "/address/all-address",
+  verifyToken,
+  productDeliveryAdress_controller.getAllProductDeliveryAddress
 );
 
+productDeliveryAdress_route.delete(
+  "/address/delete-address/:deliveryAddressId",
+  verifyToken,
+  productDeliveryAdress_controller.deleteSingleProductDeliveryAddress
+);
 
 module.exports = productDeliveryAdress_route;
