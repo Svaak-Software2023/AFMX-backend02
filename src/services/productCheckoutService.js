6b08ca8d2823656cee2eb60cc15c971629c03b02
Ashivkhan <ashivkhan347@gmail.com>
2024-03-28 15:59:55 +0530

    url changed for redirection

--- src/services/productCheckoutService.js
+++ src/services/productCheckoutService.js
@@ -89,7 +89,7 @@ const createProductCheckout = async (bodyData, paramsData, loggedInUser,res) => 
     payment_method_types: ["card"],
     line_items: combinedLineItems,
     mode: "payment",
-    return_url: `https://afmx.madextube700.com/api/session-status/{CHECKOUT_SESSION_ID}?cartId=${cart.cartId}`,
+    return_url: `https://afmx.madextube700.com/session-status/{CHECKOUT_SESSION_ID}?cartId=${cart.cartId}`,
   });
   
     // Find the largest existing productCheckoutId
